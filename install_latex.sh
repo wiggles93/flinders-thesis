18d3aec5f438ced54d344bd7011af4054dff96c4
Alastair Wiegelmann <wieg0006@flinders.edu.au>
2021-02-10 23:35:31 +1030

    added install script for latex

---
+++ install_latex.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt install latexmk texlive-base texlive-binaries texlive-fonts-recommended texlive-lang-greek texlive-latex-base texlive-latex-recommended texlive-pictures texlive-plain-generic texlive-bibtex-extra texlive-latex-extra texlive-science tex-common tex-gyre
