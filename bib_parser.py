5e5c91bc2036f3052e9e8f413db4eff3fc004a13
Alastair Wiegelmann <wieg0006@flinders.edu.au>
2021-02-12 23:52:20 +1030

    script to ensure bib files dont have bad chars

---
+++ bib_parser.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+import sys
+
+replace_chars = ["&", "%", "_", "$"]
+
+def find_all(source, search):
+    occurances = [] # list of indexes where character has occured in the string provided
+    start = 0
+    while True:
+        index = source.find(search, start)
+        if index == -1:
+            return occurances
+        else:
+            start = index + 1
+            if source[index - 1] == '\\':
+                continue
+            occurances.append(index)
+            
+
+def replace_all(locations, source):
+    for index in locations:
+        source = source[:index] + '\\' + source[index:]
+    return source
+
+try:
+    file = sys.argv[1]
+    with open(file, 'r+') as bib:
+        read_bib = bib.read()
+        for char in replace_chars:
+            replace_index = find_all(read_bib, char)
+            read_bib = replace_all(replace_index, read_bib)
+        bib.seek(0)
+        bib.write(read_bib)
+        bib.truncate()
+        
+except IndexError:
+    print("not enough parameters for script")
+# except:
+#     print("Something went bung")
+
--- updateBib.sh
+++ updateBib.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-cp /mnt/d/alastair/Documents/Uni/OneDrive\ -\ Flinders/Year\ 4/Semester\ 2/Honours/Reference/honours\ thesis.txt /home/alastair/thesis/honoursThesis.bib
+cp /home/alastair/thesis/flinders-thesis/honoursThesis.bib /home/alastair/thesis/flinders-thesis/honoursThesis.bib_old
+cp /mnt/d/alastair/Documents/Uni/OneDrive\ -\ Flinders/Year\ 4/Semester\ 2/Honours/Reference/honours\ thesis.txt /home/alastair/thesis/flinders-thesis/honoursThesis.bib
+
+python3 /home/alastair/thesis/flinders-thesis/bib_parser.py /home/alastair/thesis/flinders-thesis/honoursThesis.bib
