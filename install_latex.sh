06a462d32ded6aa2989962427844082778cd9767
Alastair Wiegelmann <wieg0006@flinders.edu.au>
2021-02-12 16:43:18 +1030

    lit review additions

--- install_latex.sh
+++ install_latex.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
 sudo apt install latexmk texlive-base texlive-binaries texlive-fonts-recommended texlive-lang-greek texlive-latex-base texlive-latex-recommended texlive-pictures texlive-plain-generic texlive-bibtex-extra texlive-latex-extra texlive-science tex-common tex-gyre
