0faa41cae053a53a3e2a0838f43f2c6b01eb4d0d
Alastair Wiegelmann <wieg0006@flinders.edu.au>
2021-02-16 23:56:45 +1030

    spellcheck performed

--- install_latex.sh
+++ install_latex.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install latexmk texlive-base texlive-binaries texlive-fonts-recommended texlive-lang-greek texlive-latex-base texlive-latex-recommended texlive-pictures texlive-plain-generic texlive-bibtex-extra texlive-latex-extra texlive-science tex-common tex-gyre
+sudo apt install latexmk texlive-base texlive-binaries texlive-fonts-recommended texlive-lang-greek texlive-latex-base texlive-latex-recommended texlive-pictures texlive-plain-generic texlive-bibtex-extra texlive-latex-extra texlive-science tex-common tex-gyre aspell
