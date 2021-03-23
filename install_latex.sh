82d860d60007bc4f2b8066d1ed9963630d458d91
Alastair Wiegelmann <wieg0006@flinders.edu.au>
2021-03-23 10:58:37 +1030

    install script updated

--- install_latex.sh
+++ install_latex.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y latexmk texlive-base texlive-binaries texlive-fonts-recommended texlive-lang-greek texlive-latex-base texlive-latex-recommended texlive-pictures texlive-plain-generic texlive-bibtex-extra texlive-latex-extra texlive-extra-utils texlive-science tex-common tex-gyre aspell
+sudo apt install -y latexmk texlive-base texlive-xetex texlive-binaries texlive-fonts-recommended texlive-lang-greek texlive-latex-base texlive-latex-recommended texlive-pictures texlive-plain-generic texlive-bibtex-extra texlive-latex-extra texlive-extra-utils texlive-science tex-common tex-gyre aspell
